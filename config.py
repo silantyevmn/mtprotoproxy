d80eb132e7a21bc976a40248c02bc1136b0b9a5a
silantyevmn <30726814+silantyevmn@users.noreply.github.com>
2026-09-21 21:57:58 +0300

    Update user secret and enable modes in config

--- config.py
+++ config.py
@@ -2,17 +2,17 @@ PORT = 443
 
 # name -> secret (32 hex chars)
 USERS = {
-    "tg":  "00000000000000000000000000000001",
+    "tg":  "ee473ce5d4958eb5f968c87680a23854a0676f6f676c652e636f6d",
     # "tg2": "0123456789abcdef0123456789abcdef",
 }
 
 MODES = {
     # Classic mode, easy to detect
-    "classic": False,
+    "classic": True,
 
     # Makes the proxy harder to detect
     # Can be incompatible with very old clients
-    "secure": False,
+    "secure": True,
 
     # Makes the proxy even more hard to detect
     # Can be incompatible with old clients
@@ -21,7 +21,7 @@ MODES = {
 
 # The domain for TLS mode, bad clients are proxied there
 # Use random existing domain, proxy checks it on start
-# TLS_DOMAIN = "www.google.com"
+TLS_DOMAIN = "www.google.com"
 
 # Tag for advertising, obtainable from @MTProxybot
 # AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
